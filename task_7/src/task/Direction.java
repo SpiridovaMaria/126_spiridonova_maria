f8c361bd32727de1cf937aa1f56d85eb0c5a8a75
SpiridovaMaria <lexus754@mail.ru>
2021-11-04 13:14:55 +0300

    task_7

---
+++ task_7/src/task/Direction.java
@@ -0,0 +1,28 @@
+package task;
+
+public enum Direction {
+    North{
+        public Vector2 move(Vector2 start, double distance){
+            return new Vector2(start.getX()+0,start.getY()-distance);
+        }
+    },
+    West{
+        public Vector2 move(Vector2 start, double distance){
+            return new Vector2(start.getX()-distance,start.getY()+0);
+        }
+    },
+    South{
+        public Vector2 move(Vector2 start, double distance){
+            return new Vector2(start.getX()+0,start.getY()+distance);
+        }
+    },
+    East{
+        public Vector2 move(Vector2 start, double distance){
+            return new Vector2(start.getX()+distance,start.getY()+0);
+        }
+    };
+
+
+   public abstract Vector2 move(Vector2 start, double distance);
+
+}
