4149f1a1334ea32b0d6de45ad970498ba8944c98
SpiridovaMaria <lexus754@mail.ru>
2021-12-26 19:25:59 +0300

    addDoc_addDocWithNullNumber_ThrowsException()

--- task_10/src/Tests/doc_tests.java
+++ task_10/src/Tests/doc_tests.java
@@ -3,6 +3,8 @@ package Tests;
 import org.junit.*;
 import Core.*;
 
+import java.util.Locale;
+
 public class doc_tests extends Assert {
     @Test
     public void create_CreateEmptyDocBook_DocCountEqualsZero(){
@@ -12,7 +14,13 @@ public class doc_tests extends Assert {
     @Test
     public void addDoc_addDocWithNumberAndDate_DocCountEqualsOne(){
         DocBook docBook = DocBook.create();
-        docBook.addDoc("number","20211226");
+        docBook.addDoc("number","date");
         assertEquals(1,docBook.getDocCount());
+    }
+    @Test
+    public void addDoc_addDocWithNullNumber_ThrowsException(){
+        DocBook docBook = DocBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> docBook.addDoc(null,"date"));
+        assertTrue(exc.getMessage().toLowerCase().contains("number cannot be null"));
     }
 }
