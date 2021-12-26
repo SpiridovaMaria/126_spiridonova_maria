1d399360c688fd96c51ac964e5cb34f6ea230c68
SpiridovaMaria <lexus754@mail.ru>
2021-12-26 11:53:14 +0300

    add addDoc_addDocWithNumberAndDate_DocCountEqualsOne()

--- task_10/src/Tests/doc_tests.java
+++ task_10/src/Tests/doc_tests.java
@@ -9,4 +9,10 @@ public class doc_tests extends Assert {
         DocBook docBook = DocBook.create();
         assertEquals(0,docBook.getDocCount());
     }
+    @Test
+    public void addDoc_addDocWithNumberAndDate_DocCountEqualsOne(){
+        DocBook docBook = DocBook.create();
+        docBook.addDoc("number","20211226");
+        assertEquals(1,docBook.getDocCount());
+    }
 }
