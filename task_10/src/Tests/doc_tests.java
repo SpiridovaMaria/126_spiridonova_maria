df4d5a4be22420be63c931674b0dd9799b7346bc
SpiridovaMaria <lexus754@mail.ru>
2021-12-26 11:36:18 +0300

    Создан тест create_CreateEmptyDocBook_DocCountEqualsZero()

--- task_10/src/Tests/doc_tests.java
+++ task_10/src/Tests/doc_tests.java
@@ -1,6 +1,12 @@
 package Tests;
 
 import org.junit.*;
+import Core.*;
 
 public class doc_tests extends Assert {
+    @Test
+    public void create_CreateEmptyDocBook_DocCountEqualsZero(){
+        DocBook docBook = DocBook.create();
+        assertEquals(0,docBook.getDocCount());
+    }
 }
