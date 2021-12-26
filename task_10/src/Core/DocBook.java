d70d2293b086d4b2bbed97c4d3f8d5ef7eb8709d
SpiridovaMaria <lexus754@mail.ru>
2021-12-26 12:00:47 +0300

    test complete

--- task_10/src/Core/DocBook.java
+++ task_10/src/Core/DocBook.java
@@ -1,8 +1,17 @@
 package Core;
 
 public class DocBook {
+    private int docCount;
+
+    private DocBook(){
+        docCount=0;
+    }
+
+    public void addDoc(String number, String date){
+        docCount++;
+    }
     public int getDocCount(){
-        return 0;
+        return docCount;
     }
     public static DocBook create(){
         return new DocBook();
